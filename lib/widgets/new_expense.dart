7bb500afcd275f02f73dbad73aa8de7e3f772322
Do Nhat <dmnhat.pt@gmail.com>
2024-01-17 11:12:54 +0700

    Using TextEditingController for handling user input.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,11 +10,7 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
-
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
-  }
+  final _titleController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +19,7 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('Title'),
@@ -33,7 +29,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_enteredTitle);
+                  print(_titleController.text);
                 },
                 child: const Text('Save Expense'),
               )
@@ -42,5 +38,11 @@ class _NewExpenseState extends State<NewExpense> {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 }
