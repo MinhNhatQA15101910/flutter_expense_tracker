08342b9f1ba7b320e1471759814ca4b88c3eeba5
Do Nhat <dmnhat.pt@gmail.com>
2024-01-17 11:24:23 +0700

    Closing the modal manually.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -37,7 +37,9 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             children: [
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
